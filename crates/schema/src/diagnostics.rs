ee9dfbfa39ebc8663bee5256946d052f223f63b7
Yaroslav Bolyukin <iam@lach.pw>
2026-01-09 22:48:34 +0100

    feat: highligh syntax in error report snippets

--- crates/schema/src/diagnostics.rs
+++ crates/schema/src/diagnostics.rs
@@ -1,5 +1,7 @@
 #[cfg(feature = "hi-doc")]
 use hi_doc::{Formatting, SnippetBuilder, Text};
+#[cfg(feature = "tree-sitter-highlight")]
+use tree_sitter_highlight::HighlightConfiguration;
 
 use crate::span::SimpleSpan;
 
@@ -27,6 +29,33 @@ pub struct Report {
 	pub parts: Vec<ReportPart>,
 }
 
+#[cfg(feature = "tree-sitter-highlight")]
+fn highlight(b: &mut SnippetBuilder) {
+	let language = tree_sitter_immigrant::LANGUAGE;
+	let mut config = HighlightConfiguration::new(
+		language.into(),
+		"immigrant",
+		tree_sitter_immigrant::HIGHLIGHTS_QUERY,
+		tree_sitter_immigrant::INJECTIONS_QUERY,
+		tree_sitter_immigrant::LOCALS_QUERY,
+	)
+	.expect("highlight configuration is valid");
+	config.configure(&[
+		"punctuation.bracket",
+		"keyword",
+		"property",
+		"type",
+	]);
+	b.highlight(config, |name, _str| {
+		match name {
+			1 => Formatting::rgb([255, 50, 50]),
+			2 => Formatting::rgb([50, 150, 50]),
+			3 => Formatting::rgb([120, 150, 50]),
+			_ => Formatting::listchar(),
+		}
+	});
+}
+
 impl Report {
 	pub fn new() -> Self {
 		Self::default()
@@ -35,13 +64,20 @@ impl Report {
 	pub fn to_hi_doc(self, src: &str) -> Vec<hi_doc::Source> {
 		let mut out = Vec::new();
 
+		let mut snippet_src = src.to_owned();
+		// For missing something at the end of input - create real character to point at
+		snippet_src.push(' ');
+
 		for part in self.parts {
-			let mut builder = SnippetBuilder::new(src);
+			let mut builder = SnippetBuilder::new(&snippet_src);
+			#[cfg(feature = "tree-sitter-highlight")]
+			highlight(&mut builder);
 			// TODO: other severity
 			for ele in part.annotations {
-				let mut ann =
-					builder.error(Text::fragment(format!("{}: {}", part.msg, ele.msg), Formatting::rgb([127, 127, 255])));
-				dbg!(&src);
+				let mut ann = builder.error(Text::fragment(
+					format!("{}: {}", part.msg, ele.msg),
+					Formatting::rgb([127, 127, 255]),
+				));
 				if ele.span.start == ele.span.end {
 					// FIXME: They shouldn't be inclusive by default
 					ann = ann.range(ele.span.start as usize..=ele.span.end as usize);
@@ -58,7 +94,7 @@ impl Report {
 		self.parts.iter().any(|p| p.severity == Severity::Error)
 	}
 
-	pub fn error(&mut self, msg: impl AsRef<str>) -> PartBuilder {
+	pub fn error(&mut self, msg: impl AsRef<str>) -> PartBuilder<'_> {
 		let part = ReportPart {
 			msg: msg.as_ref().to_owned(),
 			severity: Severity::Error,
